93cb6debbb89b04a65cacf36123d071e33556eef
anuppaul1845@gmail.com <anup-paul>
2021-04-15 17:32:58 +0600

    connect-with-database

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config();
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4mhth.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
 const port = 7000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('its working!')
 })
 
-app.listen(port)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("travelAgency").collection("features");
+  console.log("Connected successfully");
+});
+
+app.listen(process.env.PORT || port)
